e1d1a7193fd5a03ee3bf4b21910e16aa802f29a9
TheShamRio <xsham@inbox.ru>
2024-10-14 23:05:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/practicekotlinfundamentals/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.practicekotlinfundamentals
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.practicekotlinfundamentals.ui.theme.PracticeKotlinFundamentalsTheme
+
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    if (numberOfMessages > 99) {
+        println("Your phone is blowing up! You have 99+ notifications.")
+    } else {
+        println("You have $numberOfMessages notifications.")
+    }
+}
